50ee66c9ceed1827830f26220344e7aaf2d93936
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-10-21 21:36:51 -0300

    feat: pokemon cards info

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Vue 3 + Vite
+# PokeVue
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Pokedex criada utilizando Vue.JS 3.
+Tecnologias e Libs utilizadas.
 
-## Recommended IDE Setup
+- Javascript
+- Vue.Js
+- Sass
+- Axios
+- Gitart-Vue-Dialog
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+## Como testar o código?
+
+### Clone este repositório
+
+$ git clone <https://github.com/lhenriquedeveloper/vuePoke/>
+
+### Acesse a pasta do projeto no terminal/cmd
+
+$ cd vuePoke
+
+### Instale as dependências
+
+$ npm install
+or
+$ yarn
+
+### Execute a aplicação em modo de desenvolvimento
+
+$ npm run dev
+or
+$ yarn dev
+
+### Acesse o localhost na porta indicada pelo seu terminal
--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ export default {
         this.selectedPokemon = response.data
         this.dialogState = true
       })
- }
+ },
 },
 
 computed:{
@@ -80,14 +80,35 @@ computed:{
     <div class="wrapper">
       <div class="content">
         <div class="title">{{selectedPokemon.species.name}}</div>
-        <img :src="selectedPokemon.sprites.front_default" :alt="selectedPokemon.species.name">
-        <p>
-        </p>
+        <img class="pokecard-img" :src="selectedPokemon.sprites.front_default" :alt="selectedPokemon.species.name">
+        <div 
+        v-for="type in selectedPokemon.types"
+        :key="type.slot"
+        :class="`poketype--${type.type.name}`"
+        ><span class="s-infopoke">{{type.type.name}}</span>
+      </div>
+        <div class="pokeinfo">
+          <h3 class="h-infopoke">Height: <span class="spanpoke">{{(selectedPokemon.height + 2.74).toFixed(2)}} cm</span></h3>
+          <h3 class="h-infopoke">Weight: <span class="spanpoke">{{(selectedPokemon.weight + 0.45359237).toFixed(0)}} kgs</span></h3>
+        </div>
+        <div class="moves-area">
+          <h2>Pokemon Abillities</h2>
+          <table>
+            <tr>
+              <th>Move</th>
+              <th>Level</th>
+            </tr>
+            <tr v-for="move in selectedPokemon.moves" :key="move.move.name">
+              <td>{{move.move.name}}</td>
+              <td>{{move.version_group_details[0].level_learned_at}}</td>
+            </tr>
+          </table>
+        </div>
       </div>
 
       <div class="actions">
         <button
-          class="btn btn--outline-gray"
+          class="close-btn"
           @click="dialogState = false"
         >
           Close
@@ -97,15 +118,14 @@ computed:{
   </GDialog>
 </template>
 
+<!-- General Page Style  -->
 <style lang="sass">
 *
   padding: 0 
   margin: 0
-
 body
   background-color: #242424
   font-family: 'Roboto', sans-serif
-
   a
     text-decoration: none
     transition: 0.2s opacity
@@ -113,14 +133,17 @@ body
     a:hover
         opacity: 0.7
         color: #535bf2
-
-.search-area
-  width: 100%
-  display: flex
-  justify-content: center
-  align-content: center
-  margin-top: 20px
-  margin-bottom: 20px
+    </style>
+  
+<!-- Content Page Style -->
+  <style lang="sass">
+  .search-area
+    width: 100%
+    display: flex
+    justify-content: center
+    align-content: center
+    margin-top: 20px
+    margin-bottom: 20px
   input
     width: 300px
     height: 40px
@@ -136,7 +159,6 @@ body
     height: 35px
     margin-left: 10px
     cursor: pointer
-
 .pokemon-container
   display: flex
   flex-wrap: wrap
@@ -171,25 +193,373 @@ body
         color: red
 </style>
 
+<!-- Dialog Style -->
 <style scoped>
 .wrapper {
   color: #000;
 }
-
 .content {
   padding: 20px;
 }
-
 .title {
   font-size: 30px;
   font-weight: 700;
   margin-bottom: 20px;
 }
-
 .actions {
   display: flex;
   justify-content: flex-end;
   padding: 10px 20px;
   border-top: 1px solid rgba(0, 0, 0, 0.12);
 }
+</style>
+
+<!-- Content Dialog Style -->
+<style lang="sass">
+.pokecard-img
+  width: 100%
+  height: 100%
+  margin-bottom: 20px
+
+.pokeinfo
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  margin-bottom: 20px
+  h3
+    font-size: 1.2em
+    font-weight: 500
+    margin-bottom: 10px
+  .spanpoke
+    font-size: 0.8em
+    font-weight: 500
+    color: #646cff
+.moves-area
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center 
+  margin-bottom: 20px 
+  h2
+    font-size: 1.2em
+    font-weight: bold
+    margin-bottom: 10px
+  table
+    border-collapse: collapse
+    width: 100%
+    th
+      border: 1px solid #ddd
+      padding: 8px
+      text-align: center
+    td
+      border: 1px solid #ddd
+      padding: 8px
+      text-align: center
+.close-btn
+  background-color: #fff
+  border: none
+  border-radius: 5px
+  padding: 10px 20px
+  font-size: 16px
+  cursor: pointer
+  box-shadow: 0 0 5px rgba(0,0,0,0.2)
+  border: 1px solid #fff
+  transition: 0.2s opacity
+  &:hover
+    opacity: 0.7
+    color: red
+
+</style>
+<!-- Pokemon Color Type -->
+<style lang="sass">
+.poketype
+  &--bug
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #729f3f 50%, #729f3f 50%)
+    background: -ms-linear-gradient(180deg, #729f3f 50%, #729f3f 50%)
+    background: -moz-linear-gradient(180deg, #729f3f 50%, #729f3f 50%)
+    background: -o-linear-gradient(180deg, #729f3f 50%, #729f3f 50%)
+    background: linear-gradient(180deg, #729f3f 50%, #729f3f 50%)
+    background-color: #729f3f
+    color: #fff
+  &--dark
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -ms-linear-gradient(180deg, #707070 50%, #707070 50%)
+    background: -moz-linear-gradient(180deg, #707070 50%, #707070 50%)
+    background: -o-linear-gradient(180deg, #707070 50%, #707070 50%)
+    background: -webkit-linear-gradient(180deg, #707070 50%, #707070 50%)
+    background: linear-gradient(180deg, #707070 50%, #707070 50%)
+    background-color: #707070
+    color: #fff
+  &--dragon
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)
+    background: -ms-linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)
+    background: -moz-linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)
+    background: -o-linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)
+    background: linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)
+    background-color: #53a4cf
+    color: #fff
+  &--electric
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #eed535 50%, #eed535 50%)
+    background: -ms-linear-gradient(180deg, #eed535 50%, #eed535 50%)
+    background: -moz-linear-gradient(180deg, #eed535 50%, #eed535 50%)
+    background: -o-linear-gradient(180deg, #eed535 50%, #eed535 50%)
+    background: linear-gradient(180deg, #eed535 50%, #eed535 50%)
+    background-color: #eed535
+    color: #212121
+  &--fairy
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #fdb9e9 50%, #fdb9e9 50%)
+    background: -ms-linear-gradient(180deg, #fdb9e9 50%, #fdb9e9 50%)
+    background: -moz-linear-gradient(180deg, #fdb9e9 50%, #fdb9e9 50%)
+    background: -o-linear-gradient(180deg, #fdb9e9 50%, #fdb9e9 50%)
+    background: linear-gradient(180deg, #fdb9e9 50%, #fdb9e9 50%)
+    background-color: #fdb9e9
+    color: #212121
+  &--fighting
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #d56723 50%, #d56723 50%)
+    background: -ms-linear-gradient(180deg, #d56723 50%, #d56723 50%)
+    background: -moz-linear-gradient(180deg, #d56723 50%, #d56723 50%)
+    background: -o-linear-gradient(180deg, #d56723 50%, #d56723 50%)
+    background: linear-gradient(180deg, #d56723 50%, #d56723 50%)
+    background-color: #d56723
+    color: #fff
+  &--fire
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%)
+    background: -ms-linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%)
+    background: -moz-linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%)
+    background: -o-linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%)
+    background: linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%)
+    background-color: #fd7d24
+    color: #fff
+  &--flying
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)
+    background: -ms-linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)
+    background: -moz-linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)
+    background: -o-linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)
+    background: linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)
+    background-color: #3dc7ef
+    color: #212121
+  &--ghost
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #7b62a3 50%, #7b62a3 50%)
+    background: -ms-linear-gradient(180deg, #7b62a3 50%, #7b62a3 50%)
+    background: -moz-linear-gradient(180deg, #7b62a3 50%, #7b62a3 50%)
+    background: -o-linear-gradient(180deg, #7b62a3 50%, #7b62a3 50%)
+    background: linear-gradient(180deg, #7b62a3 50%, #7b62a3 50%)
+    background-color: #7b62a3
+    color: #fff
+  &--grass
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #9bcc50 50%, #9bcc50 50%)
+    background: -ms-linear-gradient(180deg, #9bcc50 50%, #9bcc50 50%)
+    background: -moz-linear-gradient(180deg, #9bcc50 50%, #9bcc50 50%)
+    background: -o-linear-gradient(180deg, #9bcc50 50%, #9bcc50 50%)
+    background: linear-gradient(180deg, #9bcc50 50%, #9bcc50 50%)
+    background-color: #9bcc50
+    color: #212121
+  &--ground
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)
+    background: -ms-linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)
+    background: -moz-linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)
+    background: -o-linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)
+    background: linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)
+    background-color: #f7de3f
+    color: #212121
+  &--ice
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #51c4e7 50%, #51c4e7 50%)
+    background: -ms-linear-gradient(180deg, #51c4e7 50%, #51c4e7 50%)
+    background: -moz-linear-gradient(180deg, #51c4e7 50%, #51c4e7 50%)
+    background: -o-linear-gradient(180deg, #51c4e7 50%, #51c4e7 50%)
+    background: linear-gradient(180deg, #51c4e7 50%, #51c4e7 50%)
+    background-color: #51c4e7
+    color: #212121
+  &--normal
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #a4acaf 50%, #a4acaf 50%)
+    background: -ms-linear-gradient(180deg, #a4acaf 50%, #a4acaf 50%)
+    background: -moz-linear-gradient(180deg, #a4acaf 50%, #a4acaf 50%)
+    background: -o-linear-gradient(180deg, #a4acaf 50%, #a4acaf 50%)
+    background: linear-gradient(180deg, #a4acaf 50%, #a4acaf 50%)
+    background-color: #a4acaf
+    color: #212121
+  &--poison
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #b97fc9 50%, #b97fc9 50%)
+    background: -ms-linear-gradient(180deg, #b97fc9 50%, #b97fc9 50%)
+    background: -moz-linear-gradient(180deg, #b97fc9 50%, #b97fc9 50%)
+    background: -o-linear-gradient(180deg, #b97fc9 50%, #b97fc9 50%)
+    background: linear-gradient(180deg, #b97fc9 50%, #b97fc9 50%)
+    background-color: #b97fc9
+    color: #fff
+  &--psychic
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #f366b9 50%, #f366b9 50%)
+    background: -ms-linear-gradient(180deg, #f366b9 50%, #f366b9 50%)
+    background: -moz-linear-gradient(180deg, #f366b9 50%, #f366b9 50%)
+    background: -o-linear-gradient(180deg, #f366b9 50%, #f366b9 50%)
+    background: linear-gradient(180deg, #f366b9 50%, #f366b9 50%)
+    background-color: #f366b9
+    color: #fff
+  &--rock
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #a38c21 50%, #a38c21 50%)
+    background: -ms-linear-gradient(180deg, #a38c21 50%, #a38c21 50%)
+    background: -moz-linear-gradient(180deg, #a38c21 50%, #a38c21 50%)
+    background: -o-linear-gradient(180deg, #a38c21 50%, #a38c21 50%)
+    background: linear-gradient(180deg, #a38c21 50%, #a38c21 50%)
+    background-color: #a38c21
+    color: #fff
+  &--steel
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%)
+    background: -ms-linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%)
+    background: -moz-linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%)
+    background: -o-linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%)
+    background: linear-gradient(180deg, #9eb7b8 50%, #9eb7b8 50%)
+    background-color: #9eb7b8
+    color: #212121
+  &--water
+    border: 1px solid #333
+    border-radius: 5px
+    font-style: italic
+    align-content: center
+    width: 50px
+    height: 20px
+    margin: 5px
+    padding: 5px
+    background: -webkit-linear-gradient(180deg, #4592c4 50%, #4592c4 50%)
+    background: -ms-linear-gradient(180deg, #4592c4 50%, #4592c4 50%)
+    background: -moz-linear-gradient(180deg, #4592c4 50%, #4592c4 50%)
+    background: -o-linear-gradient(180deg, #4592c4 50%, #4592c4 50%)
+    background: linear-gradient(180deg, #4592c4 50%, #4592c4 50%)
+    background-color: #4592c4
+    color: #fff
 </style>
